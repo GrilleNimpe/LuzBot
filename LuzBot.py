b92e8768bdb845c0c7c6cf75e02161fc8a63fddf
Boubli <49991411+realBoubli@users.noreply.github.com>
2022-06-20 23:48:01 +0200

    Add a LUT for simple messages
    Remove that shitty "if" waterfall and use a LUT

--- LuzBot.py
+++ LuzBot.py
@@ -32,51 +32,44 @@ async def on_member_update(before, after):
     else:
         await changement.send(content=f"{before.display_name} est devenue {after.display_name} !")
 
+messagesLUT = {
+    "!lpsdd" : "https://discord.gg/HyQ9mPd5",
+    "!farkdateproduction" : "https://www.youtube.com/channel/UC0f0m2dVJSu9TXkh23Unhhw/featured",
+    "!6" : "Saucisse",
+    "!7" : "Chaussette",
+    "!toto" : "Patate au BEUR",
+    "!beur" : "Brassière est un robot BIP BOUP",
+    "!koopafdp" : "🍆",
+    "!bonluz" : "!help",
+    "!help" :   "> Luz peut souhaiter la bienvenue lorsque qu'un nouveaux membre se joint à La Tavola"+"\n"+
+                "> Luz peut dire adieux à ceux qui nous quitte (triste)"+"\n"+
+                "> Luz prévient les autres membres lorsqu'ils changent de pseudo afin de ne pas les prêter à confusion"+"\n"+
+                "`!toto` pour avoir son nom de code"+"\n"+
+                "`!farkdateproduction` pour avoir accès à une chaîne youtube tenu par des chads"+"\n"+
+                "`!lpsdd` pour rejoindre l'élite de la société"+
+                "\n"+"`!inf/surv/plombier` pour BEUR"+"\n"+
+                "`!6` saucisse"+"\n"+
+                "`!7` chaussette"+"\n"+
+                "`!beur` rasberry BIP BOUP"+"\n"+
+                "`!koopafdp` : chef de l'élite de la société"+"\n"+
+                "`!bonluz` pour luz :)",
+    "!code" :   "Salle à manger : Perm"+"\n"+
+                "Cuisine : CDI"+"\n"+
+                "Patate : session"+"\n"+
+                "Patate au plomb : plombier"+"\n"+
+                "Viande : Veyon"+"\n"+
+                "Cuire une patate : Shutdown un pc"+
+                "\n"+"Patate au BEUR : toto"+"\n"+
+                "Lipo : technicien"+"\n"+
+                "BEUR : Banssière est un robot"+"\n"+
+                "IEUC : Ivan est un cyborg"+"\n"+
+                "EEUC : Ethan est un capteur"
+}
 
 @client.event
 async def on_message(message):
-    if message.content.lower() == "!lpsdd":
-        await message.channel.send("https://discord.gg/HyQ9mPd5")
-    if message.content.lower() == "!farkdateproduction":
-        await message.channel.send("https://www.youtube.com/channel/UC0f0m2dVJSu9TXkh23Unhhw/featured")
-    if message.content.lower() == "!6":
-        await message.channel.send("Saucisse")
-    if message.content.lower() == "!7":
-        await message.channel.send("Chaussette")
-    if message.content.lower() == "!toto":
-        await message.channel.send("Patate au BEUR")
-    if message.content.lower() == "!beur":
-        await message.channel.send("Brassière est un robot BIP BOUP")
-    if message.content.lower() == "!koopafdp":
-        await message.channel.send("🍆")
-    if message.content.lower() == "!bonluz":
-       await message.channel.send(file=discord.File('unknown.gif'))
-    if message.content.lower() == "!help":
-        await message.channel.send("> Luz peut souhaiter la bienvenue lorsque qu'un nouveaux membre se joint à La Tavola"+"\n"+
-                                   "> Luz peut dire adieux à ceux qui nous quitte (triste)"+"\n"+
-                                   "> Luz prévient les autres membres lorsqu'ils changent de pseudo afin de ne pas les prêter à confusion"+"\n"+
-                                   "`!toto` pour avoir son nom de code"+"\n"+
-                                   "`!farkdateproduction` pour avoir accès à une chaîne youtube tenu par des chads"+"\n"+
-                                   "`!lpsdd` pour rejoindre l'élite de la société"+
-                                   "\n"+"`!inf/surv/plombier` pour BEUR"+"\n"+
-                                   "`!6` saucisse"+"\n"+
-                                   "`!7` chaussette"+"\n"+
-                                   "`!beur` rasberry BIP BOUP"+"\n"+
-                                   "`!koopafdp` : chef de l'élite de la société"+"\n"+
-                                   "`!bonluz` pour luz :)")
-    if message.content.lower() == "!code":
-        await message.channel.send("Salle à manger : Perm"+"\n"+
-                                   "Cuisine : CDI"+"\n"+
-                                   "Patate : session"+"\n"+
-                                   "Patate au plomb : plombier"+"\n"+
-                                   "Viande : Veyon"+"\n"+
-                                   "Cuire une patate : Shutdown un pc"+
-                                   "\n"+"Patate au BEUR : toto"+"\n"+
-                                   "Lipo : technicien"+"\n"+
-                                   "BEUR : Banssière est un robot"+"\n"+
-                                   "IEUC : Ivan est un cyborg"+"\n"+
-                                   "EEUC : Ethan est un capteur")
+    cmdmsg = message.content.lower()
+    if (cmdmsg.startswith("!") and messagesLUT.get(cmdmsg) != None):
+        await message.channel.send(messagesLUT.get(cmdmsg))
 
-
-    
 client.run() #Saisir la clé du bot
