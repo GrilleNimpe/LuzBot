dcc8619e7e7a42bc85b550abac82d1fb7b229bde
Losprunios <107355374+GrilleNimpe@users.noreply.github.com>
2023-05-06 20:29:06 +0200

    Update fonction.py
    Luz 1.0.1

--- fonction.py
+++ fonction.py
@@ -24,3 +24,20 @@ def save(chaine):
     with open("commands.txt","a",encoding="utf-8") as f:
         f.write(chaine)
         f.close()
+messagesLUT = {
+    "!farkdateproduction" :
+        "https://www.youtube.com/channel/UC0f0m2dVJSu9TXkh23Unhhw",
+    "!help" :
+        """
+`!commands` To list the commands
+`!create !command : answer` Please respect the spaces
+> Luz has some really cool commands
+> Luz can ask a question to start a conversation
+> Luz doesn't look like that but he's a real chad, he just can't clean.
+> Luz can play any game you want (even those that don't exist)
+        """,
+    "!command":
+        "ANSWER",
+    "!command_2":
+        "ANSWER_2"
+}
