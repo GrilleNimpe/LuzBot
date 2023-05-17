2dee1f1d546b1e13026616e8525f7a7f2a74ca4f
Losprunios <107355374+GrilleNimpe@users.noreply.github.com>
2023-05-17 10:19:14 +0200

    Update fonction.py

--- fonction.py
+++ fonction.py
@@ -10,16 +10,6 @@ def command(cara):
 def answer(cara):
     return(cara[11+len(command(cara)):])
 
-def question():
-    with open('question.txt', 'r+') as f1:
-            qst = f1.readline().strip()
-            lines = f1.readlines()
-            f1.seek(0)
-            f1.writelines(lines[0:])
-            f1.truncate()
-            f1.close()
-    return(qst)
-
 def save(chaine):
     with open("commands.txt","a",encoding="utf-8") as f:
         f.write(chaine)
